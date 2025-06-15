9c04085dca8e7293cf18bb42dd1c13a6633abe89
Terry MacDonald <terry.macdonald@cosive.com>
2025-06-15 21:56:12 +1200

    Change the build process to avoid using LibraryWrapper
    This will mean I need to handle more in my application (DisplayMagician) but I won't need to duplicate the helper DLL as well. In my use case I will only need the single DLL to interface with the IntelControlLib.dll which should make it fairly simple.

---
+++ change_enum_to_uint.ps1
@@ -0,0 +1,21 @@
+﻿
+$bindingPath = "..\Bindings"
+
+Write-Output "Changing all *_flag_t objects to uints in folder $bindingPath...";
+
+Get-ChildItem -Path $bindingPath -Recurse -Filter '*_flag_t.cs' | ForEach-Object {
+    $file = $_.FullName
+    Write-Host "Patching file: $file"
+
+    $content = Get-Content $file
+
+    # Replace public enum XYZ { ... → public enum XYZ : uint { ...
+    $patched = $content -replace '(?<=public enum\s+\w+)\s*\{', ' : uint {'
+
+    if ($patched -ne $content) {
+        Set-Content $file $patched
+        Write-Host "  -> Patched to use : uint"
+    } else {
+        Write-Host "  -> No changes needed"
+    }
+}
